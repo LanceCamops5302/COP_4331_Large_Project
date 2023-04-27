e92e1210ca3f068439a556379584dce5e1fd327b
SilveriteKey <50954654+SilveriteKey@users.noreply.github.com>
2023-04-26 22:57:59 -0400

    Added grid and working API for clips

--- mern/client/src/components/clips.js
+++ mern/client/src/components/clips.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Typography } from '@mui/material';
 import Card from './Card'
-import { useState } from 'react';
 import axios from 'axios';
+import './VideoGrid.css';
 
 
 export default function Clips(props) {
     const [items, setItems] = useState([]);
   
     
-    axios.get('http://localhost:5000/record')
-      .then(response => {
-        setItems(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
+
+    // This method fetches the records from the database.
+    useEffect(() => {
+      async function getItems() {
+        const response = await fetch("http://localhost:5000/clips");
+  
+        if (!response.ok) {
+          const message = `An error occured: ${response.statusText}`;
+          window.alert(message);
+          return;
+        }
+  
+        const items = await response.json();
+        setItems(items);
+      }
+  
+      getItems();
+  
+      return;
+    }, [items.length]);
+
+
+    console.log(items.length);
 
   return (
     <div>
@@ -27,10 +43,10 @@ export default function Clips(props) {
           </Typography>
         </div>
         <div class="container">
-  <div className="row">
-    {items.map((item, i) => (
-      <div key={i} className="col-md-4 mb-4">
-        <Card item={item} className="card" />
+  <div className="video-grid">
+    {items.map((item) => (
+      <div>
+        <VideoItem item={item} className="card" />
       </div>
     ))}
   </div>
@@ -39,19 +55,30 @@ export default function Clips(props) {
     </div>
   );
 }
-function Item(props){
-  const { name, address, imagelink, hours } = props.item;
+const VideoItem = (props) => {
+  const { name, url, imagelink, hours } = props.item;
 
+    const videoID = extractVideoId(url);
+    const youtubeID = 'https://www.youtube.com/embed/' + videoID + '?enablejsapi=0&modestbranding=1'
+    console.log(youtubeID);
     return(
-        <div className="card p-2" style={{ width: '18rem' }}>
-        <div className="card-body">
-          <h5 className="card-title">{name}</h5>
-          <p className="card-text">c
-          {`${address.street}, ${address.city}, ${address.state} ${address.zipcode}`}          </p>
-          {/* <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a> */}
+      <div> {name}
+      <div className='video-wrapper'>
+      <iframe className="video" src={youtubeID} title="YouTube video player" 
+        frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+
         </div>
       </div>
     )
+}
+
+
+function extractVideoId(link) {
+  var regex = /[?&]([^=#]+)=([^&#]*)/g;
+  var params = {};
+  var match;
+  while (match = regex.exec(link)) {
+    params[match[1]] = match[2];
+  }
+  return params['v'];
 }
