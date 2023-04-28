4b3fc9b1a8cffde9d4caf2b89a68a94b59f21949
BrianUmbrecht <123705496+BrianUmbrecht@users.noreply.github.com>
2023-04-27 21:24:33 -0400

    Made explore go to all clips

--- mern/client/src/components/Nav.js
+++ mern/client/src/components/Nav.js
@@ -10,8 +10,8 @@ export default function Nav(){
             <nav className="navbar navbar-dark bg-dark">
                 <GiSkateboard fontSize="50px" color="white" />
                 <div className="d-flex">
-                    <a className="nav-link text-light col-4" href="/content">Home <span className="sr-only">(current)</span></a>
-                    <a className="nav-link text-light col-4" href="/clips">Explore <span className="sr-only">(current)</span></a>
+                    <a className="nav-link text-light col-4" href="/">Home <span className="sr-only">(current)</span></a>
+                    <a className="nav-link text-light col-4" href="/allClips">Explore <span className="sr-only">(current)</span></a>
                     <a className="nav-link text-light col-4" href="/login">Logout <span className="sr-only">(current)</span></a>
 
                 </div>
