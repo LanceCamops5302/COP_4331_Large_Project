c5ddffa6b54429ed38e454447ecca8ffa5cbd942
BrianUmbrecht <123705496+BrianUmbrecht@users.noreply.github.com>
2023-04-27 21:28:14 -0400

    made button work

--- mern/client/src/components/content.js
+++ mern/client/src/components/content.js
@@ -2,28 +2,37 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Carousel from 'react-material-ui-carousel'
 // import { Paper, Button } from '@mui/material'
-import { useState } from 'react';
+import { useState, useEffect  } from 'react';
 import axios from 'axios';
 // import { Card } from 'react-bootstrap';
 import {Typography} from "@mui/material";
+import { Link } from "react-router-dom";
+
 export default function Content(){
     const [responseData, setResponseData] = useState([]);
 
     const [items, setItems] = useState([]);
   
-    
+    useEffect(() => {
+      let isMounted = true;
       axios.get('http://localhost:5000/record')
         .then(response => {
-          setItems(response.data);
+          if (isMounted) {
+            setItems(response.data);
+          }
         })
         .catch(error => {
           console.log(error);
         });
+        return () => {
+          isMounted = false;
+        };
+      }, []);
     
     
     return (
-      <div class="mx-auto w-75">
-        <div class="text-center">
+      <div className="mx-auto w-75">
+        <div className="text-center">
         <Typography variant="h1" component="h2">
   Featured
 </Typography>
@@ -42,7 +51,7 @@ export default function Content(){
   }
   
   function Item(props) {
-    const { name, address, imagelink, hours } = props.item;
+    const { name, address, imagelink, hours, _id} = props.item;
   
     return (
       <div>
@@ -59,16 +68,17 @@ export default function Content(){
           )}
           <div className="card-body" style={{ display: "flex", justifyContent: "center"}}>
             <div style={{paddingRight: "200px"}}>
-              <h5 className="card-title text-primary">{name}</h5>
-              <h6 className="card-text text-dark">Address</h6>
+              <h3 className="card-title text-primary">{name}</h3>
+              <h5 className="card-text text-dark">Address</h5>
               <p className="card-text">{`${address.street}, ${address.city}, ${address.state} ${address.zipcode}`}</p>
+              <Link className="btn btn-link btn-outline-primary" to={`/clips/${_id}`}>Check it Out!</Link>
             </div>
             <div style={{}}>
               <h5 className="card-text text-success">Hours</h5>
               <ul>
                 <li>Monday: {hours.monday}</li>
                 <li>Tuesday: {hours.tuesday}</li>
-                <li>Wednesday:{hours.wednesday}</li>
+                <li>Wednesday: {hours.wednesday}</li>
                 <li>Thursday: {hours.thursday}</li>
                 <li>Friday: {hours.friday}</li>
                 <li>Saturday: {hours.saturday}</li>
@@ -76,10 +86,8 @@ export default function Content(){
               </ul>
             </div>
           </div>
-          <div className="container d-flex justify-content-center">
-          <button className="btn btn-outline-primary">Check it Out!</button>
-          </div>
         </div>
       </div>
     );
   }
+  
