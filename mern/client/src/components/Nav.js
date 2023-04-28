c563d6d885b02ef489ecbe8c0f626f297b0ae10f
BrianUmbrecht <123705496+BrianUmbrecht@users.noreply.github.com>
2023-04-27 21:58:35 -0400

    Added the name of our website in the navbar and made it go to the home page whenever its clicked

--- mern/client/src/components/Nav.js
+++ mern/client/src/components/Nav.js
@@ -8,7 +8,10 @@ export default function Nav(){
     return(
         <div>
             <nav className="navbar navbar-dark bg-dark">
-                <GiSkateboard fontSize="50px" color="white" />
+                <div className="d-flex align-items-center">
+                    <GiSkateboard fontSize="50px" color="white" />
+                    <a className="nav-link text-light" href="/" style={{fontSize: "22px"}}>Skate Park Flexin'<span className="sr-only">(current)</span></a>
+                </div>
                 <div className="d-flex">
                     <a className="nav-link text-light col-4" href="/">Home <span className="sr-only">(current)</span></a>
                     <a className="nav-link text-light col-4" href="/allClips">Explore <span className="sr-only">(current)</span></a>
